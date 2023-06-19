cc9b54f6e3e79866f13c68b0e2fbb10e6633d34c
Oleksandr <vodolazskiy.alexander18@gmail.com>
2023-06-19 05:29:10 +0300

    first commit

---
+++ src/java_Git_Lab3_6/Calculator.java
@@ -0,0 +1,7 @@
+package java_Git_Lab3_6;
+
+public class Calculator {
+
+
+
+}
---
+++ src/java_Git_Lab3_6/Main.java
@@ -0,0 +1,8 @@
+package java_Git_Lab3_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(2);
+        calculator.calculateNumbers();
+    }
+}
