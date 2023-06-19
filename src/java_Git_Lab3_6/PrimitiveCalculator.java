00d8830c1132ffd9eac70281f19b31299f345874
Oleksandr <vodolazskiy.alexander18@gmail.com>
2023-06-19 06:48:38 +0300

    2 commit

--- src/java_Git_Lab3_6/PrimitiveCalculator.java
+++ src/java_Git_Lab3_6/PrimitiveCalculator.java
@@ -1,9 +1,6 @@
 package java_Git_Lab3_6;
 
 public interface PrimitiveCalculator {
-        public double add(double a, double b);
-        public double sub(double a, double b);
-        public double mul(double a, double b);
-        public double div(double a, double b);
+
 
 }
